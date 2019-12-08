d2f86127312ddd929a3b7be4d5b63ab013205bb6
Ashley Wilkes <janonderek@gmail.com>
2019-12-08 13:24:18 +0100

    Day 2, part 2.

--- 2019/02/src/main.cpp
+++ 2019/02/src/main.cpp
@@ -2,6 +2,17 @@
 #include <fstream>
 #include <iostream>
 
+//sets noun and verb to memory addresses 1 and 2 respectively, runs thus obtained program and
+//returns whether the "output" (contents of memory address 0) of the program is equal to 
+//expectedOutput
+bool testSentence( const std::vector<int>& theProgram, int noun, int verb, long long expectedOutput ) {
+  IntcodeComputer comp{ theProgram };
+  comp.write( 1, noun );
+  comp.write( 2, verb );
+  comp.runTheProgram();
+  return comp.read( 0 ) == expectedOutput;
+}
+
 int main( int argc, char* argv[] ) {
   if ( argc != 2 ) {
     std::cout << "Call like this: 'main \"input.txt\"!\n";
@@ -22,10 +33,13 @@ int main( int argc, char* argv[] ) {
     theProgram.push_back( std::stoi( substr ) );
   }
 
-  IntcodeComputer comp{ theProgram };
-  comp.write( 1, 12 );
-  comp.write( 2, 2 );
-  comp.runTheProgram();
+  long long expectedOutput = 19690720;
 
-  std::cout << comp.read( 0 ) << '\n';
+  for ( int noun = 0; noun < 100; ++noun ) {
+    for ( int verb = 0; verb < 100; ++verb ) {
+      if ( testSentence( theProgram, noun, verb, expectedOutput ) ) {
+        std::cout << noun * 100 + verb << '\n';
+      }
+    }
+  }
 }
